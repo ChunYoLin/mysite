ffa3ab8f5b85614708160725cdc370bfc7ffd460
chunyo <chunyo@Ubuntu-16.04>
2017-10-24 22:09:56 +0800

    add three model

--- budget/models.py
+++ budget/models.py
@@ -3,20 +3,52 @@ from django.db import models
 # Create your models here.
 
 class Budget(models.Model):
-    Budget_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     date = models.DateField()
 
     def __str__(self):
-        return self.Budget_name
+        return self.name
 
 class Item(models.Model):
     budget = models.ForeignKey(
         'Budget',
         on_delete=models.CASCADE,
     )
-    Item_name = models.CharField(max_length=20)
-    Description = models.CharField(max_length=200)
-    Value = models.IntegerField(default=0)
+    name = models.CharField(max_length=20)
+    value = models.IntegerField(default=0)
+    remain = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.Item_name
+        return self.name
+
+class Bank(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Investment(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Transactions(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.IntegerField(default=0)
+    
+    bank = models.ForeignKey(
+        'Bank',
+        on_delete=models.CASCADE,
+    )
+
+    item = models.ForeignKey(
+        'Item',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
--- budget/views.py
+++ budget/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
 
-from .models import Budget, Item
+from .models import Budget, Item, Bank, Investment, Transactions
 # Create your views here.
 class IndexView(generic.ListView):
 
@@ -17,18 +17,19 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         
         context = super(IndexView, self).get_context_data(**kwargs)
+        context["Bank"] = Bank.objects.all()
+        context["Transactions"] = Bank.objects.all()
         return context
     
     def add_item(self):
         
         Budget_name = self.GET["Budget_name"]
         Item_name = self.GET["Item_name"]
-        Value = self.GET["Value"]
-        Description =self.GET["Description"]
-        Item(Item_name = Item_name)
+        value = self.GET["value"]
+        Item(name = Item_name)
         
-        B = Budget.objects.get(Budget_name=Budget_name)
-        I = Item(Item_name=Item_name, Description=Description, Value=Value, budget=B)
+        B = Budget.objects.get(name=Budget_name)
+        I = Item(name=Item_name, value=value, remain=value, budget=B)
         I.save()
         return HttpResponseRedirect('/budget/') 
 
