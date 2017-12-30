0be164a43583243c4c86d234f76408e2c4e5d5b9
chunyo <r04922108@csie.ntu.edu.tw>
2017-12-30 10:22:43 +0800

    refactoring: move out function classview

--- budget/views.py
+++ budget/views.py
@@ -2,15 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
+from django.http import JsonResponse
 from django.template.defaulttags import register
 
 from collections import OrderedDict
 from datetime import date
 from datetime import timedelta
+import calendar
 
 from .base import *
 from .models import *
 # Create your views here.
+mon_ch2num = {
+    "一月": 1,
+    "二月": 2,
+    "三月": 3,
+    "四月": 4,
+    "五月": 5,
+    "六月": 6,
+    "七月": 7,
+    "八月": 8,
+    "九月": 9,
+    "十月": 10,
+    "十一月": 11,
+    "十二月": 12,
+}
 
 @register.filter
 def get_dict_item(dictionary, key):
@@ -46,6 +62,7 @@ class BudgetView(generic.DetailView):
     model = Budget
     template_name = 'budget/budget.html'
     pk_url_kwarg = "Budget_id"
+    select_day = date.today()
 
     def get_context_data(self, **kwargs):
         budget = kwargs['object']
@@ -73,153 +90,162 @@ class BudgetView(generic.DetailView):
                 choices[c_name] += e.value
         context["CHOICES"] = choices
         context["Today"] = date.today()
-        select_day = date.today()
-        select_day_list = [select_day]
+        select_day_list = [self.select_day]
         day_range = 2
         for i in range(1, day_range+1):
-            select_day_list.append(select_day-timedelta(days=i))
-            select_day_list.append(select_day+timedelta(days=i))
+            select_day_list.append(self.select_day-timedelta(days=i))
+            select_day_list.append(self.select_day+timedelta(days=i))
         select_day_list.sort()
         context["DAY_LIST"] = select_day_list 
+        max_date = calendar.monthrange(int(Year_name), mon_ch2num[budget.name])[1]
         
         return context
+
     
-    def add_debt(self, Year_name, Budget_id):
-        
-        Debt_name = self.GET["Debt_name"]
-        value = self.GET["value"]
-        Category_name = self.GET["Category_name"]
-        
-        B = Budget.objects.get(id=Budget_id)
-        for k, v in dict(CHOICES).items():
-            if v == Category_name:
-                category = k
-        debt = Debt(name=Debt_name, value=value, remain=value, budget=B, category=category)
-        debt.save()
-        return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+def add_debt(request, Year_name, Budget_id):
     
-    def pay_debt(self, Year_name, Budget_id):
+    Debt_name = request.GET["Debt_name"]
+    value = request.GET["value"]
+    Category_name = request.GET["Category_name"]
+    
+    B = Budget.objects.get(id=Budget_id)
+    for k, v in dict(CHOICES).items():
+        if v == Category_name:
+            category = k
+    debt = Debt(name=Debt_name, value=value, remain=value, budget=B, category=category)
+    debt.save()
+    return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
 
-        Debt_id = self.GET["Debt_id"]
-        budget = Budget.objects.get(id=Budget_id)
-        debt = Debt.objects.get(id=Debt_id, budget=budget)
-        debt.is_paid = True
-        for ex in debt.expenses.all():
-            ex.is_fulfill = True
-            ex.save()
-        debt.save()
-        return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+def pay_debt(request, Year_name, Budget_id):
 
-    def delete_item(self, Year_name, Budget_id):
-        
-        Item_id = self.GET['Item_id']
-        I = Item.objects.get(id=Item_id)
-        I.delete()
-        return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+    Debt_id = request.GET["Debt_id"]
+    budget = Budget.objects.get(id=Budget_id)
+    debt = Debt.objects.get(id=Debt_id, budget=budget)
+    debt.is_paid = True
+    for ex in debt.expenses.all():
+        ex.is_fulfill = True
+        ex.save()
+    debt.save()
+    return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
 
-    def add_income(self, Year_name, Budget_id):
-        
-        name = self.GET["name"] 
-        value = self.GET["value"]
-        date = self.GET["date"]
-        Bank_name = self.GET["Bank_name"]
+def delete_item(request, Year_name, Budget_id):
+    
+    Item_id = request.GET['Item_id']
+    I = Item.objects.get(id=Item_id)
+    I.delete()
+    return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
 
-        budget = Budget.objects.get(id=Budget_id)
-        bank = Bank.objects.get(name=Bank_name)
+def add_income(request, Year_name, Budget_id):
+    
+    name = request.GET["name"] 
+    value = request.GET["value"]
+    date = request.GET["date"]
+    Bank_name = request.GET["Bank_name"]
 
-        value = int(value.replace(',', ''))
-        income_remain = value
-        
-        for debt in Debt.objects.filter(budget=budget):
-            if not debt.is_paid:
-                if debt.remain > income_remain:
-                    ex_value = income_remain
-                    debt.remain -= income_remain
-                    remain = 0
-                else:
-                    ex_value = debt.remain
-                    income_remain -= debt.remain
-                    debt.remain = 0
-                    debt.is_distributed = True
-                belong_to = debt
-                expense = Expenses(
-                        name="償還_{}".format(debt.name), 
-                        value=ex_value, 
-                        date=date, 
-                        bank=bank, 
-                        belong_to=belong_to, 
-                        category=debt.category,
-                        budget=budget, 
-                        is_fulfill=False)
-                debt.save()
-                expense.save() 
+    budget = Budget.objects.get(id=Budget_id)
+    bank = Bank.objects.get(name=Bank_name)
 
-        bank.value += income_remain
-        bank.save()
-        
-        income = Incomes(
-                name=name, 
-                value=value, 
-                remain=income_remain, 
-                date=date, 
-                bank=bank, 
-                budget=budget)
-        income.save() 
+    value = int(value.replace(',', ''))
+    income_remain = value
+    
+    for debt in Debt.objects.filter(budget=budget):
+        if not debt.is_paid:
+            if debt.remain > income_remain:
+                ex_value = income_remain
+                debt.remain -= income_remain
+                remain = 0
+            else:
+                ex_value = debt.remain
+                income_remain -= debt.remain
+                debt.remain = 0
+                debt.is_distributed = True
+            belong_to = debt
+            expense = Expenses(
+                    name="償還_{}".format(debt.name), 
+                    value=ex_value, 
+                    date=date, 
+                    bank=bank, 
+                    belong_to=belong_to, 
+                    category=debt.category,
+                    budget=budget, 
+                    is_fulfill=False)
+            debt.save()
+            expense.save() 
 
-        ratio = 0.6
-        D = Deposit(name="存款_{}".format(name), ratio=ratio, budget=budget, income=income)
-        D.update()
-        D.save()
+    bank.value += income_remain
+    bank.save()
+    
+    income = Incomes(
+            name=name, 
+            value=value, 
+            remain=income_remain, 
+            date=date, 
+            bank=bank, 
+            budget=budget)
+    income.save() 
 
-        LC = LivingCost.objects.get(name="生活/娛樂費", budget=budget)
-        LC.update()
-        LC.save()
+    ratio = 0.6
+    D = Deposit(name="存款_{}".format(name), ratio=ratio, budget=budget, income=income)
+    D.update()
+    D.save()
 
-        BC = LivingCost.objects.get(name="備用" ,budget=budget)
-        BC.update()
-        BC.save()
+    LC = LivingCost.objects.get(name="生活/娛樂費", budget=budget)
+    LC.update()
+    LC.save()
 
-        return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+    BC = LivingCost.objects.get(name="備用" ,budget=budget)
+    BC.update()
+    BC.save()
 
-    def add_expense(self, Year_name, Budget_id):
-        
-        name = self.GET["name"] 
-        value = int(self.GET["value"])
-        date = self.GET["date"]
-        print(date)
-        Bank_name = self.GET["Bank_name"]
-        Category_name = self.GET["Category_name"]
+    return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
 
-        bank = Bank.objects.get(name=Bank_name)
-        budget = Budget.objects.get(id=Budget_id)
-        LC = LivingCost.objects.get(name="生活/娛樂費", budget=budget)
-        BC = LivingCost.objects.get(name="備用", budget=budget)
-        bank.value -= value
-        bank.save()
-        if(LC.remain >= value):
-            belong_to = LC
-            LC.remain -= value
-        else:
-            belong_to = BC
-            BC.remain -= value
-        for k, v in dict(CHOICES).items():
-            if v == Category_name:
-                category = k
-        expense = Expenses(
-                name=name, 
-                value=value, 
-                date=date, 
-                bank=bank, 
-                belong_to=belong_to, 
-                category=category, 
-                budget=budget, 
-                is_fulfill=True)
-        expense.save() 
-        BC.update()
-        LC.update()
-        BC.save()
-        LC.save()
-        return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+def add_expense(request, Year_name, Budget_id):
+    
+    name = request.GET["name"] 
+    value = int(request.GET["value"])
+    date = request.GET["date"]
+    print(date)
+    Bank_name = request.GET["Bank_name"]
+    Category_name = request.GET["Category_name"]
+
+    bank = Bank.objects.get(name=Bank_name)
+    budget = Budget.objects.get(id=Budget_id)
+    LC = LivingCost.objects.get(name="生活/娛樂費", budget=budget)
+    BC = LivingCost.objects.get(name="備用", budget=budget)
+    bank.value -= value
+    bank.save()
+    if(LC.remain >= value):
+        belong_to = LC
+        LC.remain -= value
+    else:
+        belong_to = BC
+        BC.remain -= value
+    for k, v in dict(CHOICES).items():
+        if v == Category_name:
+            category = k
+    expense = Expenses(
+            name=name, 
+            value=value, 
+            date=date, 
+            bank=bank, 
+            belong_to=belong_to, 
+            category=category, 
+            budget=budget, 
+            is_fulfill=True)
+    expense.save() 
+    BC.update()
+    LC.update()
+    BC.save()
+    LC.save()
+    return HttpResponseRedirect('/budget/{}/{}'.format(Year_name, Budget_id)) 
+
+def show_prev_ex(request, Year_name, Budget_id):
+    BudgetView.select_day-=timedelta(days=2)
+    print(BudgetView.select_day)
+    data = {'a': 1}
+    return JsonResponse(data)
+    
+
 
 def asset_transfer(request):
     
